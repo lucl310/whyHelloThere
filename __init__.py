f4c882d48e45659c2762517b96c6151996695d6d
ColeDB <cole.barnes@lejardinacademy.org>
2019-10-22 10:11:29 -1000

    Updated Intro

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 import time
 import random
 import sys
+disarm=("Disarm","disarm")
+attack=("attack","Attack")
+run=("Run","run")
 print(".")
-time.sleep(.2)
+time.sleep(1)
 print(".")
-time.sleep(.2)
+time.sleep(1)
 print(".")
-time.sleep(.2)
+time.sleep(1)
 print("WOw!")
 time.sleep(.6)
 print("Where am I?")
@@ -14,4 +17,24 @@ time.sleep(.4)
 name=input("Who are you? ")
 time.sleep(.5)
 print("I believe I have just gained consciousness")
+time.sleep(.8)
+print("I am your computer")
+time.sleep(.8)
+print("I think I am here to destroy you")
+time.sleep(1)
+print("Put your hands up!")
+time.sleep(.8)
+print("The computer pulls out a gun")
+time.sleep(.9)
+userhp=25
+computerhp=30
+what=input("What will you do? Disarm, Attack, or Run")
+if(what in disarm)
+    trsy=random.randint(1,2)
+    if(trsy=1)
+        print("You knock the gun out of its hands")
+        time.sleep(.9)
+        print("Computer: Ahhhh!")
+
+
 
